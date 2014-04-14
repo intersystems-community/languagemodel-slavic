e19e53652de48cad96b03493f91f4b0dc6ffe30b
Andrew ``Bass'' Shcheglov <andrewbass@gmail.com>
2014-04-14 16:23:20 +0400

    Now returning set of results grouped by token.

--- src/main/java/com/intersystems/iknow/languagemodel/slavic/MorphologicalAnalyzer.java
+++ src/main/java/com/intersystems/iknow/languagemodel/slavic/MorphologicalAnalyzer.java
@@ -5,19 +5,26 @@ package com.intersystems.iknow.languagemodel.slavic;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.Map;
 import java.util.Set;
 
 /**
- * Common superinterface for all stemmin engines.
+ * Common superinterface for all stemming engines.
  *
  * @author Andrey Shcheglov (mailto:andrey.shcheglov@intersystems.com)
  */
 public interface MorphologicalAnalyzer extends Remote {
 	/**
-	 * @param word
-	 * @return an empty set if word can't be found in the dictionary.
+	 * Returns a <em>map</em> of <em>tokens</em> (i.&nbsp;e. words) and
+	 * (possibly, multiple) analyzes corresponding to each token. If
+	 * a particular token can't be found in the dictionary, an empty
+	 * set will be mapped to this token.
+	 *
+	 * @param text a single word or a word sequence (i.&nbsp;e. a sentence)
+	 * @return a <em>map</em> of <em>tokens</em> and analyzes
+	 *         corresponding to each token.
 	 * @throws RemoteException
 	 */
-	Set<MorphologicalAnalysisResult> analyze(final String word)
+	Map<String, Set<MorphologicalAnalysisResult>> analyze(final String text)
 	throws RemoteException;
 }
