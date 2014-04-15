91b2f35c1c1eadd8ee2a2c72c1adcb4774ab3d0b
Andrew ``Bass'' Shcheglov <andrewbass@gmail.com>
2014-04-15 18:19:05 +0400

    Infinitive and imperative added

--- src/main/java/com/intersystems/iknow/languagemodel/slavic/categories/GrammaticalTense.java
+++ src/main/java/com/intersystems/iknow/languagemodel/slavic/categories/GrammaticalTense.java
@@ -9,7 +9,9 @@ import com.intersystems.iknow.languagemodel.slavic.GrammaticalCategory;
  * @author Andrey Shcheglov (mailto:andrey.shcheglov@intersystems.com)
  */
 public enum GrammaticalTense implements GrammaticalCategory {
+	INFINITIVE,
 	PAST,
 	PRESENT,
 	FUTURE,
+	IMPERATIVE,
 }
